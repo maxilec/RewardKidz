2445fa40cf111312cb0ea4d35379fa4dc872d86c
maxilec <maxilec@users.noreply.github.com>
2026-03-29 22:45:54 +0000

    deploy: a275fa517c0830a691d49588187c62c63e6a01e7

---
+++ firebase.config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBXdLGC0PbhIVrTPY6eXY4p9SVFwz_5LXI",
+  authDomain: "rewardkidz-4fe68.firebaseapp.com",
+  projectId: "rewardkidz-4fe68",
+  storageBucket: "rewardkidz-4fe68.firebasestorage.app",
+  messagingSenderId: "755878661989",
+  appId: "1:755878661989:web:5a63bf5355f56bbb36db8c"
+};
